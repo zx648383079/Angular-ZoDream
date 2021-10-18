6b3b9929082ed522df8c83e947e36e330ffa0e06
ZoDream <648383079@qq.com>
2021-10-18 22:40:57 +0800

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiEndpoint: 'https://zodream.cn/open/',
-  assetUri: 'https://zodream.cn',
+  apiEndpoint: 'http://zodream.localhost/open/',
+  assetUri: 'http://zodream.localhost',
   appid: '11543906547',
   secret: '012e936d3d3653b40c6fc5a32e4ea685',
 };
