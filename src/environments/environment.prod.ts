8975759b4639a4dc4de7f2ca4b40e41bc9d6d863
ZoDream <648383079@qq.com>
2023-03-18 11:45:20 +0800

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
     production: true,
-    apiEndpoint: 'https://zodream.cn/open/',
-    assetUri: 'https://zodream.cn',
-    appid: '11543906547',
-    secret: '012e936d3d3653b40c6fc5a32e4ea685',
-    editorKey: 's4frjuyhg4kew7tv6sv8iivqxvmyp8m2wwd1560rk2bv26qe',
+    apiEndpoint: '<api base uri>',
+    assetUri: '<asset base uri>',
+    appid: '<appid>',
+    secret: '<secret>',
+    editorKey: '<tyinmce editor key>',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
     assetUri: 'http://zodream.localhost',
     appid: '11543906547',
     secret: '012e936d3d3653b40c6fc5a32e4ea685',
-    editorKey: 's4frjuyhg4kew7tv6sv8iivqxvmyp8m2wwd1560rk2bv26qe',
+    editorKey: '',
 };
 
 /*
