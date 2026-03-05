087285b433e0ea31f010472fcf01c980b9d6a5c0
ZoDream <648383079@qq.com>
2026-03-05 20:53:13 +0800

    update

--- src/app/modules/chat/team/create/team-create.component.ts
+++ src/app/modules/chat/team/create/team-create.component.ts
@@ -1,7 +1,7 @@
 import { Component, inject, signal } from '@angular/core';
 import { IIconItem } from '../../../../theme/models/seo';
-import { ArraySource } from '../../../../components/form';
-import { form } from '@angular/forms/signals';
+import { ArraySource, ButtonEvent } from '../../../../components/form';
+import { form, required } from '@angular/forms/signals';
 import { ChatService } from '../../chat.service';
 
 @Component({
@@ -16,6 +16,8 @@ export class TeamCreateComponent {
 
     public readonly visible = signal(false);
     public readonly page = signal(0);
+
+    public readonly invalidTip = signal('');
 
 
     public readonly typeItems: IIconItem[] = [
@@ -32,9 +34,12 @@ export class TeamCreateComponent {
         type: 0,
         open_type: 0,
         open_rule: '',
-    }));
+    }), schemePath => {
+        required(schemePath.name, {message: $localize `Name is required`});
+    });
 
     private confirmFn: Function;
+    private asyncHandler = 0;
 
 
     public open(cb: () => void) {
@@ -42,14 +47,27 @@ export class TeamCreateComponent {
         this.confirmFn = cb;
     }
 
-    public close(isOk = false) {
+    public close() {
         this.visible.set(false);
         this.confirmFn();
-        if (!isOk) {
+        
+    }
+
+    public tapSubmit(e?: ButtonEvent) {
+        if (this.dataForm().invalid()) {
+            this.asyncTip(this.dataForm().errors()[0].message);
             return;
         }
-        this.service.teamCreate(this.dataForm().value()).subscribe(res => {
-
+        e?.enter();
+        this.service.teamCreate(this.dataForm().value()).subscribe({
+            next: _ => {
+                e?.reset();
+                this.page.update(v => v + 1);
+            },
+            error: err => {
+                e?.reset();
+                this.asyncTip(err.error.message);
+            }
         });
     }
 
@@ -57,5 +75,18 @@ export class TeamCreateComponent {
     public tapType(val: number) {
         this.page.set(1);
         this.dataForm.type().value.set(val);
+    }
+
+    private asyncTip(msg: string|any, time = 3000) {
+        if (this.asyncHandler > 0) {
+            clearTimeout(this.asyncHandler);
+        }
+        this.invalidTip.set(msg);
+        if (!msg) {
+            return;
+        }
+        this.asyncHandler = window.setTimeout(() => {
+            this.invalidTip.set('');
+        }, time);
     }
 }
